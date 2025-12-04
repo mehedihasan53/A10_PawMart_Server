a23f1acf7d62cb23e61cb09db56adc0ed0009263
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-12-05 02:02:56 +0600

    implement get listing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = 3000
